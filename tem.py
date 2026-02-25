e8f70b4787439bf54322c8425ebdd9d5bd2da3d0
mageshnote4-gif <mageshnote4@gmail.com>
2026-02-25 16:02:40 +0530

    Create tem.py

---
+++ tem.py
@@ -0,0 +1,8 @@
+temperature = float(input("Enter temperature value: "))
+
+if temperature > 50:
+    print("Warning! High Temperature!")
+elif temperature < 10:
+    print("Warning! Low Temperature!")
+else:
+    print("Temperature is Normal.")
